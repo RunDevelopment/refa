4138294a1200f627eccea874e96ae2f4f215e9b7
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-08-21 17:58:28 +0200

    Faster regex stress test

--- tests/regex-stress-test.ts
+++ tests/regex-stress-test.ts
@@ -2,34 +2,51 @@ import { assert } from "chai";
 import { Parser } from "../src/js";
 import { PrismRegexes } from "./helper/prism-regex-data";
 import { NFA } from "../src/nfa";
-import { DFA } from "../src/dfa";
-import { group } from "./helper/util";
+import { DFA, ReadonlyDFA } from "../src/dfa";
+import { Expression, Simple } from "../src/ast";
+
+/**
+ * Setting this to `true` will enable the check that verifies that the language of the generated RE from `toRegex` is
+ * the same as the language of the NFA/DFA that created it.
+ *
+ * The generated RE tends to create NFA that are both large and very non-deterministic. This means that the conversion
+ * to DFA will create __A LOT__ nodes (sometimes >10k). Both creating and minimizing the DFA takes time (up to a minute
+ * for a single regex).
+ *
+ * Only set this to `true` if you have the time to run it.
+ */
+const CHECK_RE_LANGUAGE = false;
+
+function equalLanguage(expected: ReadonlyDFA, re: Simple<Expression>, maxCharacter: number): void {
+	const nfa = NFA.fromRegex(re, { maxCharacter }, {
+		disableLookarounds: true
+	});
+	const dfa = DFA.fromNFA(nfa, { maxNodes: 100000 });
+	dfa.minimize();
+
+	assert.isTrue(expected.structurallyEqual(dfa));
+}
 
 describe("Regex stress test", function () {
 	this.timeout(60 * 1000); // timeout after a minute
 
-	const groupSize = 20;
-	const groups = group(PrismRegexes, groupSize);
+	PrismRegexes.forEach((literal, index) => {
+		let patternPreview = String(literal);
+		if (patternPreview.length > 80) {
+			patternPreview = patternPreview.substr(0, 80) + "...";
+		}
+		it(`[${index}]: ${patternPreview}`, function () {
+			const { expression, maxCharacter } = Parser.fromLiteral(literal).parse();
+			const nfa = NFA.fromRegex(expression, { maxCharacter }, {
+				disableLookarounds: true
+			});
+			const dfa = DFA.fromNFA(nfa);
+			dfa.minimize();
 
-	groups.forEach((g, index) => {
-		it(`Regexes ${1 + index * groupSize} to ${index * groupSize + g.length}`, function () {
+			const re = nfa.toRegex();
 
-			for (const literal of g) {
-				const doesNotThrow = <T>(fn: () => T): T => {
-					let result: T | undefined = undefined;
-					assert.doesNotThrow(() => {
-						result = fn();
-					}, `Pattern: ${literal}`);
-					return result!;
-				};
-
-				const parser = doesNotThrow(() => Parser.fromLiteral(literal));
-				const { expression, maxCharacter } = doesNotThrow(() => parser.parse());
-				const nfa = doesNotThrow(() => NFA.fromRegex(expression, { maxCharacter }, {
-					disableLookarounds: true
-				}));
-				const dfa = doesNotThrow(() => DFA.fromNFA(nfa));
-				doesNotThrow(() => dfa.minimize());
+			if (CHECK_RE_LANGUAGE) {
+				equalLanguage(dfa, re, maxCharacter);
 			}
 		});
 	});
