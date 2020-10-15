d46fcb7bb115bc0385980dcf23c807668f23b0e0
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-10-15 19:18:03 +0200

    Word: Added methods to pick readable char/words This fixes #8

--- src/words.ts
+++ src/words.ts
@@ -1,4 +1,4 @@
-import { CharSet } from "./char-set";
+import { CharRange, CharSet } from "./char-set";
 
 export function fromUTF16ToString(word: Iterable<number>): string {
 	return String.fromCharCode(...word);
@@ -97,4 +97,80 @@ function* nestedIteration<T>(arrays: T[][]): IterableIterator<T[]> {
 		}
 		yield res;
 	} while (hasNext());
+}
+
+
+const READABILITY_ASCII_PRIORITY: readonly CharRange[] = [
+	// A-Z
+	{ min: 0x41, max: 0x5A },
+	// a-z
+	{ min: 0x61, max: 0x7A },
+	// 0-9
+	{ min: 0x30, max: 0x39 },
+	// -
+	{ min: 0x2D, max: 0x2D },
+	// _
+	{ min: 0x5F, max: 0x5F },
+	// space
+	{ min: 0x20, max: 0x20 },
+	// printable ASCII
+	{ min: 0x20, max: 0x7E },
+	// tab
+	{ min: 0x09, max: 0x09 },
+	// \n
+	{ min: 0x0A, max: 0x0A },
+	// \r
+	{ min: 0x0D, max: 0x0D },
+];
+/**
+ * Returns the most humanly readable character in the given character set. Which character is picked is entirely
+ * implementation-defined but, generally, word characters will be picked over non-word characters and printable
+ * characters will be picked over non-printable characters.
+ *
+ * If the given character set is empty, `undefined` will be returned.
+ *
+ * @param set
+ */
+export function pickMostReadableCharacter(set: CharSet): number | undefined {
+	if (set.ranges.length === 0) {
+		// empty
+		return undefined;
+	} else if (set.ranges.length === 1) {
+		const { min, max } = set.ranges[0];
+		if (min === max) {
+			// we don't have any choice
+			return min;
+		}
+	}
+
+	// search in ASCII range
+	for (const range of READABILITY_ASCII_PRIORITY) {
+		const c = set.commonCharacter(range);
+		if (c !== undefined) {
+			return c;
+		}
+	}
+
+	// choose any character
+	return set.ranges[0].min;
+}
+
+/**
+ * Returns a word of the given word set that is the most humanly readable.
+ *
+ * If at least one of the given character sets is empty, `undefined` will be returned.
+ *
+ * @param wordSet
+ */
+export function pickMostReadableWord(wordSet: Iterable<CharSet>): number[] | undefined {
+	const word: number[] = [];
+	for (const set of wordSet) {
+		const c = pickMostReadableCharacter(set);
+		if (c === undefined) {
+			return undefined;
+		} else {
+			word.push(c);
+		}
+	}
+	return word;
 }
