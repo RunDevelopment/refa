84f985d803d09e462a18bda71f5fbdee27555046
albert-sun <sun831@purdue.edu>
2020-11-10 14:49:51 -0500

    basic testcases

--- tests/nfa.ts
+++ tests/nfa.ts
@@ -1,4 +1,4 @@
-import { NFA } from "../src/nfa";
+import { NFA, match } from "../src/nfa";
 import { assert } from "chai";
 import { fromStringToUnicode, fromUnicodeToString } from "../src/words";
 import { literalToString, literalToNFA, removeIndentation, reachableFinalStates, literalToDFA } from "./helper/fa";
@@ -1302,6 +1302,41 @@ describe("NFA", function () {
 			}
 		}
 	});
+
+	describe("thompson", function() {
+		test([
+			{
+				regex: /\d/,
+				input: "1",
+			},
+			{
+				regex: /\d\w/,
+				input: "1a",
+			},
+			{
+				regex: /\d\d\d*/,
+				input: "12345",
+			}
+		])
+
+		interface TestCase {
+			regex: RegExp,
+			input: string,
+		}
+
+		function test(cases: TestCase[]): void {
+			for (const {regex, input} of cases) {
+				// convert input to array of integer representation
+				let inputIterable = input.split("").map(c => c.charCodeAt(0));
+
+				// parse regex using to regex conversion
+				const nfa = literalToNFA(regex);
+				const normalMatch = nfa.test(inputIterable);
+				const thompsonMatch = nfa.match(input);
+				assert.equal(normalMatch, thompsonMatch);
+			}
+		}
+	})
 });
 
 function getWords(nfa: NFA): string[] {
