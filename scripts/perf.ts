8a7846862010aecd6dc989f036b4fa0111a2979a
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-02-25 20:39:51 +0100

    Fixed perf script

--- scripts/perf.ts
+++ scripts/perf.ts
@@ -13,31 +13,37 @@ function perfTest(): void {
 		return result;
 	}
 	function showResult(): void {
+		const maxLen = Math.max(...Object.keys(durationRecord).map(s => s.length));
 		for (const key in durationRecord) {
 			const durations = durationRecord[key];
 			const avg = durations.reduce((a, b) => a + b, 0) / durations.length;
 			const max = Math.max(...durations);
 
-			console.log(`${key}:\tavg=${Number(avg.toExponential(2))}ms\tmax=${Number(max.toExponential(2))}ms`);
+			console.log(
+				`${(key + ":").padEnd(maxLen)}\tavg=${Number(avg.toExponential(2))}ms\tmax=${Number(max.toExponential(2))}ms`
+			);
 		}
 	}
 
 	let errors = 0;
 	let counter = 0;
 	for (const literal of PrismRegexes) {
-		process.stdout.write(`\r${++counter}/${PrismRegexes.length}`);
+		counter++;
+		if (counter === 862) {
+			// this regex creates a DFA with >500k states. The DFA just doesn't fit into Node's limit of 2GB memory
+			continue;
+		}
+		process.stdout.write(`\r${counter}/${PrismRegexes.length}`);
 		if (counter === PrismRegexes.length) {
 			console.log();
 		}
 
 		try {
 			const parser = measure("Create parser", () => JS.Parser.fromLiteral(literal));
-			const { expression, maxCharacter } = measure("parse", () =>
-				parser.parse({
-					backreferences: "disable",
-					assertions: "disable",
-				})
-			);
+			const { expression, maxCharacter } = measure("parse", () => parser.parse({ backreferences: "disable", maxNodes: 100_000 }));
+			measure("toLiteral", () => JS.toLiteral(expression));
+			measure("toLiteral fast", () => JS.toLiteral(expression, { fastCharacters: true }));
+
 			const nfa = measure("Create NFA", () =>
 				NFA.fromRegex(
 					expression,
@@ -45,12 +51,23 @@ function perfTest(): void {
 					{ assertions: "disable" }
 				)
 			);
+			measure("toRegex NFA", () => nfa.toRegex({ maxNodes: 100_000 }));
 			const dfa = measure("Create DFA", () => DFA.fromFA(nfa));
 			measure("Minimize DFA", () => dfa.minimize());
+
+			measure("toRegex mDFA", () => {
+				try {
+					dfa.toRegex({ maxNodes: 100_000 })
+				} catch (error) {
+					if (!String(error).includes("Too many RE AST nodes")) {
+						throw error;
+					}
+				}
+			});
 		} catch (error) {
 			errors++;
 			console.log(`Error in ${literal}`);
-			//throw error;
+			throw error;
 		}
 	}
 
