0bce36bb1e17a5b2731328f1a94a927cceadb276
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-04-28 13:42:38 +0200

    Added more tests

--- tests/char-util.ts
+++ tests/char-util.ts
@@ -1,6 +1,7 @@
 import { assert } from "chai";
 import { CharRange, CharSet } from "../src/char-set";
 import { getBaseSets, runEncodeCharacters } from "../src/char-util";
+import { Char } from "../src/core-types";
 import { charsFromRegex } from "./helper/chars";
 import { assertEqualSnapshot } from "./helper/snapshot";
 
@@ -19,7 +20,7 @@ describe("char-util", function () {
 	});
 
 	it(getBaseSets.name, function () {
-		const regexes: RegExp[] = [/a/u, /b/u, /c/u, /\w/u, /\d/u, /\p{L}/u, /\p{Lu}/u, /\p{Ll}/u];
+		const regexes: RegExp[] = [/a/u, /b/u, /c/u, /\w/u, /\d/u, /\p{L}/u, /\p{Lu}/u, /\p{Ll}/u, /[^]/u, /[]/u];
 		const chars = regexes.map(charsFromRegex);
 
 		const cases: { id: string; sets: CharSet[] }[] = [];
@@ -41,6 +42,24 @@ describe("char-util", function () {
 
 		const actual = cases
 			.map(({ id, sets }) => {
+				const baseSets = getBaseSets(sets);
+
+				assert.isTrue(
+					baseSets.every(b => !b.isEmpty),
+					"Expected all base sets to be non-empty"
+				);
+				assert.isTrue(
+					unionAll(sets).equals(unionAll(baseSets)),
+					"Expected the union of all base sets to be equal to the union of all input sets."
+				);
+				for (let i = 0; i < baseSets.length; i++) {
+					const a = baseSets[i];
+					for (let j = i + 1; j < baseSets.length; j++) {
+						const b = baseSets[j];
+						assert.isTrue(a.isDisjointWith(b), "Expected base sets to be disjoint with each other.");
+					}
+				}
+
 				return ["Input: " + id, ...getBaseSets(sets).map(c => c.toString())].join("\n");
 			})
 			.join("\n\n");
@@ -48,3 +67,7 @@ describe("char-util", function () {
 		assertEqualSnapshot(this, actual);
 	});
 });
+
+function unionAll(sets: Iterable<CharSet>, maxCharacter: Char = 0x10ffff): CharSet {
+	return CharSet.empty(maxCharacter).union(...sets);
+}
