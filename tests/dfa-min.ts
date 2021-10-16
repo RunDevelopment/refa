77ad1fdeac4fa23abd35305bf8f1493a315daacf
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-10-16 13:29:40 +0200

    Split DFA test

---
+++ tests/dfa-min.ts
@@ -0,0 +1,83 @@
+import { DFA } from "../src/dfa";
+import { assert } from "chai";
+import { literalToString } from "./helper/fa";
+import { Parser } from "../src/js";
+import { NFA } from "../src/nfa";
+import { PrismRegexes } from "./helper/prism-regex-data";
+import { combineTransformers, transform } from "../src/ast";
+import * as Transformers from "../src/transformers";
+import { TooManyNodesError } from "../src/errors";
+import { CONFIG_RUN_STRESS_TEST } from "./helper/config";
+import { assertEqualSnapshot } from "./helper/snapshot";
+import { createHash } from "crypto";
+
+describe("DFA minimization", function () {
+	if (!CONFIG_RUN_STRESS_TEST) {
+		return;
+	}
+
+	const candidates = PrismRegexes.map((r, i) => ({ regex: r, id: i })).filter(
+		({ regex }) => regex.source.length < 1e3
+	);
+
+	function toDFA(regex: RegExp): DFA {
+		const result = Parser.fromLiteral(regex).parse({ backreferences: "disable" });
+
+		const applyTransformer = combineTransformers([
+			Transformers.inline(),
+			Transformers.removeDeadBranches(),
+			Transformers.removeUnnecessaryAssertions(),
+			Transformers.sortAssertions(),
+			Transformers.applyAssertions(),
+			Transformers.removeUnnecessaryAssertions(),
+		]);
+		const modifiedExpression = transform(applyTransformer, result.expression);
+
+		const finalExpression = transform(Transformers.patternEdgeAssertions({ remove: true }), modifiedExpression);
+
+		const nfa = NFA.fromRegex(finalExpression, result, { assertions: "disable", unknowns: "disable" });
+
+		return DFA.fromFA(nfa);
+	}
+
+	for (const { regex, id } of candidates) {
+		// node v10 has a bug where all "/" chars are escaped. This escapes all "/" chars to make the
+		// result consistent across versions.
+		const source = regex.source.replace(/([^\\](?:\\{2})*)(?=\/)/g, "$1\\");
+
+		const preview = literalToString({ source, flags: regex.flags })
+			// max length of 80
+			.replace(/^([^]{80})[^]+/, "$1...");
+
+		it(`${id}: ${preview}`, function () {
+			this.timeout(10_000);
+
+			let dfa;
+			try {
+				dfa = toDFA(regex);
+			} catch (error) {
+				if (error instanceof TooManyNodesError) {
+					return;
+				}
+				throw error;
+			}
+
+			dfa.minimize();
+
+			// minimize(minimize(dfa)) == minimize(dfa)
+			// This is just a basic sanity check
+			const copy = dfa.copy();
+			assert.isTrue(copy.structurallyEqual(dfa), "Copy is not equal to original");
+			copy.minimize();
+
+			if (!copy.structurallyEqual(dfa)) {
+				assert.equal(copy.toDot(), dfa.toDot());
+				assert.fail("structurally equal doesn't work");
+			}
+
+			// This way we can detect when the DFA minimization produces incorrect results.
+			// We could also store the DFA string itself but this results in mega bytes of snapshots.
+			assertEqualSnapshot(this, createHash("sha256").update(dfa.toString()).digest("base64"));
+		});
+	}
+});
--- tests/dfa.ts
+++ tests/dfa.ts
@@ -2,18 +2,12 @@ import { DFA } from "../src/dfa";
 import { assert } from "chai";
 import { literalToDFA, literalToNFA, literalToString, removeIndentation } from "./helper/fa";
 import { EMPTY_LITERALS, FINITE_LITERALS, NON_EMPTY_LITERALS, NON_FINITE_LITERALS } from "./helper/regexp-literals";
-import { Literal, Parser } from "../src/js";
+import { Literal } from "../src/js";
 import { fromStringToUnicode, fromUnicodeToString } from "../src/words";
 import { prefixes } from "./helper/util";
 import { testWordTestCases, wordTestData } from "./helper/word-test-data";
 import { NFA } from "../src/nfa";
-import { PrismRegexes } from "./helper/prism-regex-data";
-import { combineTransformers, transform } from "../src/ast";
-import * as Transformers from "../src/transformers";
-import { TooManyNodesError } from "../src/errors";
-import { CONFIG_RUN_STRESS_TEST } from "./helper/config";
 import { assertEqualSnapshot } from "./helper/snapshot";
-import { createHash } from "crypto";
 import { FROM_REGEX_LITERALS } from "./helper/from-regex-data";
 
 describe("DFA", function () {
@@ -157,80 +151,6 @@ describe("DFA", function () {
 				});
 			}
 		}
-
-		describe("Prism regexes", function () {
-			if (!CONFIG_RUN_STRESS_TEST) {
-				return;
-			}
-
-			const candidates = PrismRegexes.map((r, i) => ({ regex: r, id: i })).filter(
-				({ regex }) => regex.source.length < 1e3
-			);
-
-			function toDFA(regex: RegExp): DFA {
-				const result = Parser.fromLiteral(regex).parse({ backreferences: "disable" });
-
-				const applyTransformer = combineTransformers([
-					Transformers.inline(),
-					Transformers.removeDeadBranches(),
-					Transformers.removeUnnecessaryAssertions(),
-					Transformers.sortAssertions(),
-					Transformers.applyAssertions(),
-					Transformers.removeUnnecessaryAssertions(),
-				]);
-				const modifiedExpression = transform(applyTransformer, result.expression);
-
-				const finalExpression = transform(
-					Transformers.patternEdgeAssertions({ remove: true }),
-					modifiedExpression
-				);
-
-				const nfa = NFA.fromRegex(finalExpression, result, { assertions: "disable", unknowns: "disable" });
-
-				return DFA.fromFA(nfa);
-			}
-
-			for (const { regex, id } of candidates) {
-				// node v10 has a bug where all "/" chars are escaped. This escapes all "/" chars to make the
-				// result consistent across versions.
-				const source = regex.source.replace(/([^\\](?:\\{2})*)(?=\/)/g, "$1\\");
-
-				const preview = literalToString({ source, flags: regex.flags })
-					// max length of 80
-					.replace(/^([^]{80})[^]+/, "$1...");
-
-				it(`${id}: ${preview}`, function () {
-					this.timeout(10_000);
-
-					let dfa;
-					try {
-						dfa = toDFA(regex);
-					} catch (error) {
-						if (error instanceof TooManyNodesError) {
-							return;
-						}
-						throw error;
-					}
-
-					dfa.minimize();
-
-					// minimize(minimize(dfa)) == minimize(dfa)
-					// This is just a basic sanity check
-					const copy = dfa.copy();
-					assert.isTrue(copy.structurallyEqual(dfa), "Copy is not equal to original");
-					copy.minimize();
-
-					if (!copy.structurallyEqual(dfa)) {
-						assert.equal(copy.toDot(), dfa.toDot());
-						assert.fail("structurally equal doesn't work");
-					}
-
-					// This way we can detect when the DFA minimization produces incorrect results.
-					// We could also store the DFA string itself but this results in mega bytes of snapshots.
-					assertEqualSnapshot(this, createHash("sha256").update(dfa.toString()).digest("base64"));
-				});
-			}
-		});
 	});
 
 	describe("Minimize & Equal", function () {
