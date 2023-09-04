0f15aacc81d3489d2fa75855ab01c57da12abef1
Michael Schmidt <mitchi5000.ms@googlemail.com>
2023-09-04 15:55:14 +0200

    Added more tests for simplify

--- tests/transformers/simplify.ts
+++ tests/transformers/simplify.ts
@@ -3,12 +3,25 @@ import { simplify } from "../../src/transformers";
 
 describe("Transformers", function () {
 	const transformer = simplify();
+	const transformerIgnore = simplify({ ignoreAmbiguity: true, ignoreOrder: true });
 
 	describe(/[\w-]+(?=\.\w+)/i.exec(__filename)![0], function () {
-		itTest(transformer, []);
+		itTest(transformer, [
+			/\b(?!\d)\b\w+\b\s*\(/,
+			/(?:^|@)\b\w+\b/,
+			/"""(?:(?!""").)*"""/s,
+			/"""((?!""")(?:[^\\]|\\"))*"""/,
+			/<title>(?:(?!<\/title>).)*<\/title>/,
+
+			{ literal: /\b(?:true|false)\b/, transformer: transformerIgnore, stepByStep: true },
+		]);
 
 		it("Prism regex snapshot", function () {
 			regexSnapshot(this, transformer);
+		});
+
+		it("Prism regex snapshot ignoring order and ambiguity", function () {
+			regexSnapshot(this, transformerIgnore);
 		});
 	});
 });
